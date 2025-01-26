5b213c22be9f76d320fada42b6949da9ce179866
rianchain <rianziwalker@gmail.com>
2025-01-26 12:58:22 +0700

    Update WalletInputs with more chain options and input placeholder

--- frontend/src/components/WalletInputs.js
+++ frontend/src/components/WalletInputs.js
@@ -4,12 +4,20 @@ function WalletInputs({ chain, wallet, setChain, setWallet }) {
   return (
     <div>
       <h1>Input a Wallet and Chain</h1>
-      <input onChange={(e) => setWallet(e.target.value)} value={wallet}></input>
+      <input
+        onChange={(e) => setWallet(e.target.value)}
+        value={wallet}
+        placeholder="Enter wallet address"
+      ></input>
       <p>
         <span>Set Chain : </span>
         <select onChange={(e) => setChain(e.target.value)} value={chain}>
-          <option value="0x1">ETH</option>
-          <option value="0x89">Polygon</option>
+          <option value="0x1">ETH Mainnet</option>
+          <option value="0x89">Polygon Mainnet</option>
+          <option value="0x144">zkSync Era</option>
+          <option value="0xaa36a7">Sepolia</option>
+          <option value="0x6f3">OP Sepolia</option>
+          <option value="0x133">Polygon Amoy</option>
         </select>
       </p>
     </div>
